9bfff9594ea848b28d70b8c55f79d4c74bab774d
bisnin0 <67915194+bisnin0@users.noreply.github.com>
2020-07-07 15:01:35 +0900

    Add files via upload

--- MultiFTest2.java
+++ MultiFTest2.java
@@ -9,21 +9,17 @@ public class MultiFTest2 {
 		Scanner stdin = new Scanner(System.in);
 		System.out.print("숫자를 입력 : ");
 		int count = stdin.nextInt();
-		if (count < 0 )
-		{
+		if (count < 0) {
 			System.out.print("입력된 수 " + count);
 			System.out.print("은(는) 음수입니다");
-		
-		}
-		else if (count ==0)
+
+		} else if (count == 0)
 			System.out.print("입력된 수는 0 입니다");
 		else {
 			System.out.print("입력된 수 " + count);
 			System.out.print("은(는) 양수입니다.");
 		}
-					
-		
-		
+
 	}
 
 }
